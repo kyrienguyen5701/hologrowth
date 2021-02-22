c0288de11c12771d176f1985eaeb98dc8e8da6aa
Night.K <ntnam117@gmail.com>
2021-02-22 13:04:49 +0700

    + added base color handling

---
+++ src/assets/ts/colors.ts
@@ -0,0 +1,18 @@
+import { GetCSSVar, SetCSSVar } from "./common"
+
+export const COLOR_VARS = [
+    '',
+    '-tint-25',
+    '-tint-50',
+    '-shade-25',
+    '-shade-50',
+    '-complement'
+]
+
+export function ChangeColor(member: string) {
+    COLOR_VARS.forEach(colorVar => {
+        const name = `--color-${member}${colorVar}`;
+        const targetName = `--color-current${colorVar}`;
+        SetCSSVar(targetName, GetCSSVar(name));
+    });
+}
