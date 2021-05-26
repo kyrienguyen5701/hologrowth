da3370d89b88c6eaa0f8da912ef08569a38caddd
Night.K <ntnam117@gmail.com>
2021-05-26 21:43:31 +0700

    + add localize function

--- src/assets/ts/localize.ts
+++ src/assets/ts/localize.ts
@@ -1,4 +1,28 @@
+import langData from "@/assets/json/localization.json";
+import { LanguageData } from "./interfaces";
+let lang = "en";
+
 export function GetLocalizedText(text :string)
 {
+    if (Object.keys(langData).indexOf(text) != -1)
+    {
+        const textData = (langData as Record<string, LanguageData>)[text];
+        if (Object.keys(textData).indexOf(lang) != -1)
+        {
+            switch (lang) {
+                case "en":
+                    return textData.en || "";
+            
+                case "jp":
+                    return textData.jp || "";
+            }
+        }
+        return text;
+    }
     return text;
+}
+
+export function SetLang(langName: string)
+{
+    lang = langName;
 }
