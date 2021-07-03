a1b315bf36158b6987f5c78543657f1f6c5ccb9f
Kyrie Nguyen <sonnhthanglong@gmail.com>
2021-07-03 15:54:14 -0400

    + Add bgm and solo songs' paths of talents

--- server/tools/talents.py
+++ server/tools/talents.py
@@ -4,11 +4,13 @@ import pandas as pd
 
 talentsFilePath = '../../public/talents.json'
 talentsDestPath = '../../src/assets/json/talents.json'
+bgmDir = '../../src/assets/sounds/bgm'
+soloDir = '../../src/assets/sounds/solo'
 
 with open(talentsFilePath) as f:
     content = json.load(f)
 
-df = pd.DataFrame(columns=['branch', 'genNumber', 'genName', 'genOther', 'name', 'channelId'])
+df = pd.DataFrame(columns=['branch', 'genNumber', 'genName', 'genOther', 'name', 'channelId', 'bgm', 'solo'])
 
 i = 0
 for branch, branchInfo in content.items():
@@ -32,8 +34,11 @@ for branch, branchInfo in content.items():
         if len(gens) > 2:
             genOther = gens[2:]
 
-        df.loc[i] = [branch, genNum, genName, genOther, talentName, channelId]
+        bgm = os.listdir(f'{bgmDir}/{talentName}')
+        solo = os.listdir(f'{soloDir}/{talentName}')
+
+        df.loc[i] = [branch, genNum, genName, genOther, talentName, channelId, bgm, solo]
         i += 1
 
 df['genNumber'] = df['genNumber'].astype('str')
-df.to_json(talentsDestPath, orient='records', indent=4)
+df.to_json(talentsDestPath, orient='records', force_ascii=False, indent=4)
